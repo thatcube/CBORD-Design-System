5d3ca29977a7fdbedc1bcf57495c415103cd5214
thatcube <brandon-moore@live.com>
2018-07-31 16:06:54 -0400

    fixed 'jump-to' navigation issues

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 $(document).ready(function() {
     $(".menu-toggle").click(function() {
-        $(".menu, .menu-toggle, .sidebar").toggleClass("open");
+        $(".menu, .menu-toggle, .sidebar, .header, body").toggleClass("open");
     });
-    $('.menu a').click(function() {
-        $(".menu, .menu-toggle, .sidebar").toggleClass("open");
+    $('.menu a').click(function closeNav() {
+        $(".menu, .menu-toggle, .sidebar, .header, body").toggleClass("open");
     });
 });
+
+
+var $body = $('body');
+$body.on('click', function(event) {
+  var clickedOutside = $(event.target).closest('.menu-toggle').length == 0;
+  if (clickedOutside && $body.hasClass('open')) {
+    $(".menu, .menu-toggle, .sidebar, .header, body").toggleClass("open");
+  }
+})
 
 clipboard = new ClipboardJS('.copy');
 
@@ -26,11 +35,11 @@ var lastId,
       if (item.length) { return item; }
     });
 
-// Bind click handler to menu items
-// so we can get a fancy scroll animation
+// Bind click handler to menu items for scroll animation
 menuItems.click(function(e){
   var href = $(this).attr("href"),
-      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight;
+    //position where the page scrolls to for each link
+      offsetTop = href === "#" ? 0 : $(href).offset().top-75;
   $('html, body').stop().animate({ 
       scrollTop: offsetTop
   }, 300);
@@ -59,3 +68,44 @@ $(window).scroll(function(){
          .end().filter("[href='#"+id+"']").parent().addClass("active");
    }                   
 });
+
+
+
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('header').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    
+    lastScrollTop = st;
+}
